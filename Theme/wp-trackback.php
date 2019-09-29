fb5f1e6e68f78839cd84fb58f5784aace22d709f
PhamVietAnh99 <40080810+PhamVietAnh99@users.noreply.github.com>
2019-09-29 21:30:21 +0700

    Theme

--- Theme/wp-trackback.php
+++ Theme/wp-trackback.php
@@ -20,6 +20,7 @@ if ( empty( $wp ) ) {
  *
  * @since 0.71
  *
+ * @param mixed  $error         Whether there was an error.
  *                              Default '0'. Accepts '0' or '1', true or false.
  * @param string $error_message Error message if an error occurred.
  */
